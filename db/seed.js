f21e4e90acba10cca18f7893cec44fc635ff27dc
MWyssy <mike@wyss.co.uk>
2023-04-28 12:18:22 +0300

    Add task 3

--- db/seed.js
+++ db/seed.js
@@ -19,17 +19,30 @@ function seed() {
     })
     .then(() => {
       return createParks();
-    });
+    })
+    .then(() => {
+      return createRides();
+    })
 }
 
 function createParks() {
-  /* Create your parks table in the query below */
   return db.query(
     `CREATE TABLE parks (
       park_id SERIAL PRIMARY KEY,
       park_name VARCHAR(40) NOT NULL,
       year_opened INT NOT NULL,
       annual_attendance INT NOT NULL
+    )`);
+}
+
+function createRides() {
+  return db.query(
+    `CREATE TABLE rides (
+      ride_id SERIAL PRIMARY KEY,
+      park_id INT REFERENCES parks(park_id),
+      ride_name VARCHAR(40) NOT NULL,
+      year_opened DATE NOT NULL,
+      votes INT DEFAULT 0
     )`);
 }
 
