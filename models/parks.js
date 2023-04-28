26de0a4157d9074788609c2965b4aed251e91915
MWyssy <mike@wyss.co.uk>
2023-04-28 16:46:55 +0300

    add task 10

--- models/parks.js
+++ models/parks.js
@@ -8,6 +8,25 @@ exports.selectParks = () => {
         });
 };
 
-exports.updateParkById = () => {};
+exports.updateParkById = (id, updates) => {
+    return Promise.all(
+        Object.entries(updates).map((update) => {
+        return db.query(`
+            UPDATE parks
+            SET ${update[0]} = '${update[1]}'
+            WHERE park_id = ${id};
+        `)
+        })
+    )
+    .then(() => {
+        return db.query(`
+            SELECT * FROM parks
+            WHERE park_id = ${id}
+        ;`);
+    })
+    .then((updatedParks) => {
+        return updatedParks.rows[0];
+    })
+};
 
 exports.removeParkById = () => {};
